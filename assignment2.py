2939015c17380d6d4b4f548728442dea2abf71ad
igonzalez <igonzalezs@ceinmark.net>
2025-12-28 09:08:12 +0100

    Third commit

--- assignment2.py
+++ assignment2.py
@@ -3,10 +3,10 @@ while True:
         limit = int(input("Input a positive Integer to FizzBuzz: "))
         if limit < 0:
             raise Exception("The integer has to be positive.")
+        break
     except Exception as e:
         print(e)
         continue
-    break
 
 for i in range(1, limit + 1):
     if i % 3 == 0 and i % 5 == 0:
