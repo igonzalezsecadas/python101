a42643e8b6a3fa744eee92c8f7a6c2298165c3cd
igonzalez <igonzalezs@ceinmark.net>
2025-12-28 09:05:36 +0100

    Second commit

---
+++ assignment2.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        limit = int(input("Input a positive Integer to FizzBuzz: "))
+        if limit < 0:
+            raise Exception("The integer has to be positive.")
+    except Exception as e:
+        print(e)
+        continue
+    break
+
+for i in range(1, limit + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
